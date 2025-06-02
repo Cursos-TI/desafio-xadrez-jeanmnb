0c30ea347f66920c8d48fe16ee90814170efc6e5
jeanmnb <jeanmarceljmd@gmail.com>
2025-06-02 19:29:37 +0000

    bispo

--- xadrez.c
+++ xadrez.c
@@ -27,12 +27,23 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
+    
+    // - Movimento da Torre 
     int casasTorre = 5; // número de casas a percorrer
     printf("Movimento da Torre:\n");
 
     for (int i = 1; i <= casasTorre; i++) {
         printf("Direita\n");
+    }
+
+    // -Movimento do Bispo
+    int casasBispo = 5;
+    int contadorBispo = 1;
+    printf("\nMovimento do Bispo:\n");
+
+    while (contadorBispo <= casasBispo) {
+        printf("Cima Direita\n");
+        contadorBispo++;
     }
 
     return 0;
